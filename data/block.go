5a8561e483eff42266c72250210059a7bbd2a1af
Brian Kidney <bkidney@briankidney.ca>
2015-05-08 22:58:46 -0230

    Adds a create method.

--- data/block.go
+++ data/block.go
@@ -1,5 +1,11 @@
 package data
 
+import (
+	"crypto/rand"
+	"fmt"
+	"math"
+)
+
 type block struct {
 	/*
 	   magic - 6
@@ -12,11 +18,33 @@ type block struct {
 	   padding
 	*/
 	magic       [6]byte
-	block_len   int16
-	num_links   int16
+	block_len   int8
+	num_links   int8
 	offset      int32
 	content_len int32
-	link        []byte
+	links       []link
 	content     []byte
 	padding     []byte
+}
+
+func (blk *block) create(block_size int, links []link, content []byte) {
+
+	blk.magic = [6]byte{0x5, 0x4, 0x3, 0x2, 0x1, 0x0}
+	blk.block_len = int8(math.Log2(float64(block_size)))
+	blk.num_links = int8(len(links))
+	blk.offset = int32(blk.num_links)*96 + 16
+	blk.content_len = int32(len(content))
+
+	blk.links = links
+	blk.content = content
+
+	padding_len := block_size - 16 - int(blk.content_len) - (int(blk.num_links) * 96)
+
+	padding := make([]byte, padding_len)
+	_, err := rand.Read(padding)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
